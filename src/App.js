a8ae8f3fc75a17dd684e6b06c91ccc655d4aa8fb
datma <datma113112111@gmail.com>
2021-07-23 13:52:50 +0700

    regex...

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ import MyCustomInput from './components/MyCustomInput'
 function App() {
   const LABEL_NAME = "Enter your Name"
   const LABEL_AGE = "Enter your Age"
-  
+
+  const ID_NAME = 'name'
+  const ID_AGE = 'age'
+
+
+
+  const checkRegexOfName = (value) => {
+    const MESSAGE_REGEX_NAME = "Name must only be character"
+    const REGEX = /[\d]/g
+    
+    let matches = REGEX.test(value)
+    console.log(matches)
+
+    return matches ?  MESSAGE_REGEX_NAME : ""
+  }
+  const checkRegexOfAge = (value) => {
+    const MESSAGE_REGEX_AGE = "age must only be number"
+    const REGEX = /[A-Za-z]/g
+    
+    let matches = REGEX.test(value)
+    return matches ? MESSAGE_REGEX_AGE : ""
+  }
+
   return (
     <div className="App container mt-5">
       <div className="col-4">
-      <MyCustomInput labelOfInput={LABEL_NAME} />
-        <MyCustomInput labelOfInput={LABEL_AGE} />
-        
+        <MyCustomInput labelOfInput={LABEL_NAME} idOfInput={ID_NAME}  checkRegex={checkRegexOfName}  />
+        <MyCustomInput labelOfInput={LABEL_AGE}  idOfInput={ID_AGE}   checkRegex={checkRegexOfAge}   />
+
       </div>
     </div>
   );
--- src/components/MyCustomInput.js
+++ src/components/MyCustomInput.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const MyCustomInput = ({ labelOfInput }) => {
+const MyCustomInput = ({ labelOfInput, idOfInput, checkRegex }) => {
+    const [messageOfRegexError, setmessageOfRegexError] = useState("")
+    const notMatchRegex = (valueOfInput) => {
+        return valueOfInput ? "" : "d-none"
+    }
+
+
     return (
         <div className="my-input-container mb-3">
-            <input type="text" id="myInputText" className="my-input-text col-12" placeholder=" " 
-               spellCheck={false}
+            <input
+                type="text"
+                id={idOfInput}
+                className="my-input-text col-12"
+                placeholder=" "
+                spellCheck={false}
+                autoComplete="off"
+                onBlur={(event) => {
+                    setmessageOfRegexError(checkRegex(event.target.value))
+                }}
             />
-            <label htmlFor="myInputText" className="my-input-label">
+            <label htmlFor={idOfInput} className="my-input-label">
                 {" "}
                 {labelOfInput}{" "}
             </label>
+            <p style={{color: "red"}} className={notMatchRegex(messageOfRegexError)}> {messageOfRegexError} </p>
         </div>
     );
 };
