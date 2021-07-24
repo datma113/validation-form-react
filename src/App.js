2be1fa0169a86131898c04ee7cd97ed097b44e9e
datma <datma113112111@gmail.com>
2021-07-24 09:10:00 +0700

    optimize

--- src/App.js
+++ src/App.js
@@ -1,41 +1,66 @@
-import './App.css';
-import MyCustomInput from './components/MyCustomInput'
+import "./App.css";
+import MyCustomInput from "./components/MyCustomInput";
 
 function App() {
-  const LABEL_NAME = "Enter your Name"
-  const LABEL_AGE = "Enter your Age"
+    const inputComponents = [
+        { label: "Enter your Name", messageRegex: "" },
+        { label: "Enter your Age", messageRegex: "" },
+    ];
 
-  const ID_NAME = 'name'
-  const ID_AGE = 'age'
+    const regexModule = (function () {
+        const REGEX_OF_NAME = /[\d]/;
+        const REGEX_OF_AGE = /[A-Za-z]/;
 
+        const MESSAGE_OF_REGEX_NAME = "Only Character";
+        const MESSAGE_OF_REGEX_AGE = "Only Number";
 
+        //return message(string)
+        function checkRegexOfAnyText(anyText, regex, message) {
+            let matches = regex.test(anyText);
+            return matches ? message : "";
+        }
 
-  const checkRegexOfName = (value) => {
-    const MESSAGE_REGEX_NAME = "Name must only be character"
-    const REGEX = /[\d]/g
-    
-    let matches = REGEX.test(value)
-    console.log(matches)
+        return {
+            checkRegexOfName: function (name) {
+                return checkRegexOfAnyText(name, REGEX_OF_NAME, MESSAGE_OF_REGEX_NAME);
+            },
+            checkRegexOfAge: function (age) {
+                return checkRegexOfAnyText(age, REGEX_OF_AGE, MESSAGE_OF_REGEX_AGE);
+            },
+        };
+    })();
 
-    return matches ?  MESSAGE_REGEX_NAME : ""
-  }
-  const checkRegexOfAge = (value) => {
-    const MESSAGE_REGEX_AGE = "age must only be number"
-    const REGEX = /[A-Za-z]/g
-    
-    let matches = REGEX.test(value)
-    return matches ? MESSAGE_REGEX_AGE : ""
-  }
+    const inputComponentsMap = inputComponents.map((inputComponent, index) => {
+        const REGEX_OF_NAME = 0;
+        const REGEX_OF_AGE = 1;
 
-  return (
-    <div className="App container mt-5">
-      <div className="col-4">
-        <MyCustomInput labelOfInput={LABEL_NAME} idOfInput={ID_NAME}  checkRegex={checkRegexOfName}  />
-        <MyCustomInput labelOfInput={LABEL_AGE}  idOfInput={ID_AGE}   checkRegex={checkRegexOfAge}   />
+        let checkRegex = () => {};
 
-      </div>
-    </div>
-  );
+        switch (index) {
+            case REGEX_OF_NAME:
+                checkRegex = regexModule.checkRegexOfName;
+                break;
+            case REGEX_OF_AGE:
+                checkRegex = regexModule.checkRegexOfAge;
+                break;
+            default:
+                break;
+        }
+        return (
+            <MyCustomInput
+                labelOfInput={inputComponent.label}
+                idOfInput={index}
+                checkRegex={checkRegex}
+                key={index}
+            />
+        );
+    });
+
+    return (
+        <div className="App container mt-5">
+            <div className="col-4"> {inputComponentsMap} </div>
+        </div>
+    );
 }
 
 export default App;
